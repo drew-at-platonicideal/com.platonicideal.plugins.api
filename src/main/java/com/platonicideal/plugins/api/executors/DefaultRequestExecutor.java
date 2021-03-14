be1b5d44a1334089f8fb79ae896324ffb4a728f9
drew-at-titus <57359899+drew-at-titus@users.noreply.github.com>
2021-03-14 15:04:29 -0400

    Building out an internally resource-limited request executor
    In order to avoid overwhelming clients, a resource-limited request
    executor. First pass ensures that, at most, four requests will have been
    submitted at once.

--- src/main/java/com/platonicideal/plugins/api/executors/DefaultRequestExecutor.java
+++ src/main/java/com/platonicideal/plugins/api/executors/DefaultRequestExecutor.java
@@ -1,51 +1,34 @@
 package com.platonicideal.plugins.api.executors;
 
-import java.io.IOException;
-import java.util.function.Supplier;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpResponse;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.platonicideal.plugins.api.Response;
-import com.platonicideal.plugins.api.ResponseContentExtractor;
 
 @Service
 public class DefaultRequestExecutor implements RequestExecutor {
 
-    private static final Logger LOG = LoggerFactory.getLogger(DefaultRequestExecutor.class);
-    
-    private final Supplier<CloseableHttpClient> clientSupplier;
-    private final ResponseContentExtractor contentExtractor;
-    private final CurlDisplayer curlDisplayer;
+    private CallableRequest.Factory callableRequestFactory;
+    private ExecutorService executor;
+
 
     @Autowired
-    public DefaultRequestExecutor(Supplier<CloseableHttpClient> clientSupplier, ResponseContentExtractor contentExtractor, CurlDisplayer curlDisplayer) {
-        this.clientSupplier = clientSupplier;
-        this.contentExtractor = contentExtractor;
-        this.curlDisplayer = curlDisplayer;
+    public DefaultRequestExecutor(CallableRequest.Factory callableRequestFactory) {
+        this.callableRequestFactory = callableRequestFactory;
+        this.executor = Executors.newFixedThreadPool(4);
     }
 
     public Response execute(HttpUriRequestBase request) {
-        LOG.debug("Executing {}", curlDisplayer.getCurlFor(request));
-        
-        long start = System.currentTimeMillis();
-        try (CloseableHttpClient client = clientSupplier.get();
-             CloseableHttpResponse httpResponse = client.execute(request)) {
-            long duration = System.currentTimeMillis() - start;
-                Response response = Response.buildFrom(httpResponse, contentExtractor);
-                LOG.debug("Response was {} ({}) for {} {} in {}ms", response.getCode(), response.getReason(), request.getMethod(), request.getPath(), duration);
-                if(!response.isSuccessful()) {
-                    LOG.error("Error Response body: {}", response.getContent());
-                } else {
-                    LOG.trace("Response content {}", response.getContent());
-                }
-                return response;
-        } catch (IOException ex) {
+        Callable<Response> callable = callableRequestFactory.wrap(request);
+        try {
+            return executor.submit(callable).get();
+        } catch (InterruptedException | ExecutionException ex) {
             throw new IllegalStateException(
                     "Failed during the execution of " + request.getMethod() + " " + request.getPath(), ex);
         }
