83729acda92bcab330dbe36070aaabe1162367b6
Drew Stevens <wastevens@gmail.com>
2023-02-26 15:16:10 -0500

    Added support for uploading files

--- src/main/java/com/platonicideal/plugins/api/RequestBuilder.java
+++ src/main/java/com/platonicideal/plugins/api/RequestBuilder.java
@@ -1,5 +1,6 @@
 package com.platonicideal.plugins.api;
 
+import java.io.File;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
 import java.nio.charset.Charset;
@@ -13,7 +14,10 @@ import java.util.stream.Collectors;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 import org.apache.hc.client5.http.entity.UrlEncodedFormEntity;
+import org.apache.hc.client5.http.entity.mime.FileBody;
+import org.apache.hc.client5.http.entity.mime.MultipartEntityBuilder;
 import org.apache.hc.core5.http.ContentType;
+import org.apache.hc.core5.http.HttpEntity;
 import org.apache.hc.core5.http.io.entity.StringEntity;
 import org.apache.hc.core5.http.message.BasicNameValuePair;
 
@@ -27,20 +31,23 @@ public class RequestBuilder {
     private final Map<String, String> parameters;
     private final Map<String, String> formValues;
     private final String entity;
+    private final HttpEntity fileEntity;
     private final ContentType contentType;
 
     private RequestBuilder(String host, RequestMethod method) {
-        this(host, method, new HashMap<>(), new HashMap<>(), new HashMap<>(), null, null);
+        this(host, method, new HashMap<>(), new HashMap<>(), new HashMap<>(), null, null, null);
     }
     
     private RequestBuilder(String host, RequestMethod method, Map<String, String> headers,
-            Map<String, String> parameters, Map<String, String> formValues, String entity, ContentType contentType) {
+            Map<String, String> parameters, Map<String, String> formValues, String entity, HttpEntity fileEntity, 
+            ContentType contentType) {
         this.url = host;
         this.method = method;
         this.headers = headers;
         this.parameters = parameters;
 		this.formValues = formValues;
         this.entity = entity;
+		this.fileEntity = fileEntity;
         this.contentType = contentType;
     }
 
@@ -64,26 +71,26 @@ public class RequestBuilder {
     }
     
     public RequestBuilder withParameter(String key, String value) {
-        return new RequestBuilder(url, method, headers, cloneWith(parameters, key, value), formValues, entity, contentType);
+        return new RequestBuilder(url, method, headers, cloneWith(parameters, key, value), formValues, entity, fileEntity, contentType);
     }
     
 	public RequestBuilder withParameter(String key, int value) {
-		return new RequestBuilder(url, method, headers, cloneWith(parameters, key, String.valueOf(value)), formValues, entity, contentType);
+		return new RequestBuilder(url, method, headers, cloneWith(parameters, key, String.valueOf(value)), formValues, entity, fileEntity, contentType);
 	}
 	
 	public RequestBuilder withFormValue(String key, String value) {
-		return new RequestBuilder(url, method, headers, parameters, cloneWith(formValues, key, String.valueOf(value)), entity, contentType);
+		return new RequestBuilder(url, method, headers, parameters, cloneWith(formValues, key, String.valueOf(value)), entity, fileEntity, contentType);
 	}
 	
 	public RequestBuilder withFormValue(String key, Optional<String> value) {
 		if(value.isPresent()) {
-			return new RequestBuilder(url, method, headers, parameters, cloneWith(formValues, key, String.valueOf(value.get())), entity, contentType);
+			return new RequestBuilder(url, method, headers, parameters, cloneWith(formValues, key, String.valueOf(value.get())), entity, fileEntity, contentType);
 		}
-		return new RequestBuilder(url, method, headers, parameters, formValues, entity, contentType);
+		return new RequestBuilder(url, method, headers, parameters, formValues, entity, fileEntity, contentType);
 	}
     
     public RequestBuilder withHeader(String key, String value) {
-        return new RequestBuilder(url, method, cloneWith(headers, key, value), parameters, formValues, entity, contentType);
+        return new RequestBuilder(url, method, cloneWith(headers, key, value), parameters, formValues, entity, fileEntity, contentType);
     }
     
     private Map<String, String> cloneWith(Map<String, String> source, String key, String value) {
@@ -97,13 +104,20 @@ public class RequestBuilder {
     }
     
     public RequestBuilder withBody(String entity, ContentType contentType) {
-        return new RequestBuilder(url, method, headers, parameters, formValues, entity, contentType);
+        return new RequestBuilder(url, method, headers, parameters, formValues, entity, fileEntity, contentType);
+    }
+    
+    public RequestBuilder withFile(File file) {
+    	HttpEntity fileEntity = MultipartEntityBuilder.create().addPart("file", new FileBody(file)).build();
+    	return new RequestBuilder(url, method, headers, parameters, formValues, entity, fileEntity, ContentType.MULTIPART_FORM_DATA);
     }
     
     public HttpUriRequestBase build() {
         HttpUriRequestBase request = method.request(url());
         headers.forEach((k, v) -> request.addHeader(k, v));
-        if(!formValues.isEmpty()) {
+        if(fileEntity != null) {
+        	request.setEntity(fileEntity);
+        } else if(!formValues.isEmpty()) {
         	request.setEntity(new UrlEncodedFormEntity(formValues.entrySet().stream().map(e -> new BasicNameValuePair(e.getKey(), e.getValue())).collect(Collectors.toList())));
         } else if(entity != null) {
             request.setEntity(new StringEntity(entity, contentType));
