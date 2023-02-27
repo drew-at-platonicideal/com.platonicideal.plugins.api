ad881cd3f6fc6d92e721cb0a4804d70732a82274
Drew Stevens <wastevens@gmail.com>
2023-02-26 20:36:59 -0500

    Renamed RateLimitedRequestExecutor to ThreadedRequestExecutor

--- src/main/java/com/platonicideal/plugins/api/executors/ThreadedRequestExecutor.java
+++ src/main/java/com/platonicideal/plugins/api/executors/ThreadedRequestExecutor.java
@@ -7,12 +7,12 @@ import org.apache.hc.client5.http.classic.methods.HttpUriRequestBase;
 
 import com.platonicideal.plugins.api.Response;
 
-public class RateLimitedRequestExecutor implements RequestExecutor {
+public class ThreadedRequestExecutor implements RequestExecutor {
 
     private final RequestExecutor delegate;
     private final ExecutorService executor;
 
-    public RateLimitedRequestExecutor(RequestExecutor delegate, ExecutorService executor) {
+    public ThreadedRequestExecutor(RequestExecutor delegate, ExecutorService executor) {
         this.delegate = delegate;
         this.executor = executor;
     }
